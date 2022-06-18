3127e84915502d7fd7949774625f8a9705e9f370
jonathanleivag <email@jonathanleivag.cl>
2022-06-18 17:19:57 -0400

    configuración de vscode, eslint y stylelint

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint'
+import StylelintPlugin from 'vite-plugin-stylelint'
+
+export default defineConfig({
+  plugins: [eslintPlugin(), StylelintPlugin()]
+})
